44832ffed95ba08779c084e46f79c913a2358671
jonathan-robertson <jrob356@icloud.com>
2022-06-28 13:27:12 -0500

    implement functions in console command

--- OnlyThreeChances/Commands/ConsoleCmdOnlyThreeChances.cs
+++ OnlyThreeChances/Commands/ConsoleCmdOnlyThreeChances.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlyThreeChances.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,10 @@ namespace OnlyThreeChances.Commands {
         private static readonly string[] Commands = new string[] {
             "onlyThreeChances",
             "otc"
+        };
+
+        private static readonly string[] Options = new string[] {
+            "MaxLives"
         };
 
         public override string[] GetCommands() {
@@ -24,18 +29,85 @@ namespace OnlyThreeChances.Commands {
             int j = 1;
             return $@"Usage:
   {i++}. {GetCommands()[0]}
-  {i++}. {GetCommands()[0]} TODO
-  {i++}. {GetCommands()[0]} TODO
-  {i++}. {GetCommands()[0]} TODO
+  {i++}. {GetCommands()[0]} list
+  {i++}. {GetCommands()[0]} config <{string.Join(" / ", Options)}> <value>
+  {i++}. {GetCommands()[0]} update <user id / player name / entity id> <remainingLives>
 Description Overview
-{j++}. TODO
-{j++}. TODO
-{j++}. TODO
-{j++}. TODO";
+{j++}. View current mod options
+{j++}. List remaining lives for all players
+{j++}. Configure a given option
+{j++}. Update a specific player's remaining lives";
         }
 
         public override void Execute(List<string> _params, CommandSenderInfo _senderInfo) {
-            throw new NotImplementedException();
+            try {
+                if (_params.Count == 0) {
+                    SdtdConsole.Instance.Output($"MaxLives: {Config.MaxLives}");
+                    return;
+                }
+                switch (_params[0].ToLower()) {
+                    case "list":
+                        if (_params.Count != 1) {
+                            break;
+                        }
+                        if (GameManager.Instance.World.Players.Count == 0) {
+                            SdtdConsole.Instance.Output("There are no players currently online");
+                            return;
+                        }
+                        GameManager.Instance.World.Players.list.ForEach(p => {
+                            SdtdConsole.Instance.Output($"{p.GetCVar(Values.LivesRemainingCvar)}/{p.GetCVar(Values.MaxLivesCVar)} remaining for {p.GetDebugName()} ({p.entityId})");
+                        });
+                        return;
+                    case "config":
+                        if (_params.Count != 3) {
+                            break;
+                        }
+                        HandleConfig(_params);
+                        return;
+                    case "update":
+                        if (_params.Count != 3) {
+                            break;
+                        }
+                        HandleUpdate(_params);
+                        return;
+                    default:
+                        SdtdConsole.Instance.Output("Invald parameter provided");
+                        return;
+                }
+                SdtdConsole.Instance.Output("Invald request");
+            } catch (Exception e) {
+                SdtdConsole.Instance.Output($"Exception encountered: \"{e.Message}\"\n{e.StackTrace}");
+            }
+        }
+
+        private void HandleConfig(List<string> _params) {
+            switch (_params[1].ToLower()) {
+                case "maxlives":
+                    if (int.TryParse(_params[2], out int value)) {
+                        Config.SetMaxLives(value);
+                        SdtdConsole.Instance.Output($"Successfully updated; MaxLives set to {value}");
+                    } else {
+                        SdtdConsole.Instance.Output("Unable to parse value: must be of type int");
+                    }
+                    break;
+                default:
+                    SdtdConsole.Instance.Output("Invald parameter provided");
+                    break;
+            }
+        }
+
+        private void HandleUpdate(List<string> _params) {
+            if (!int.TryParse(_params[2], out int livesRemaining)) {
+                SdtdConsole.Instance.Output("Unable to parse value: must be of type int");
+                return;
+            }
+            ClientInfo clientInfo = ConsoleHelper.ParseParamIdOrName(_params[1], true, false);
+            if (clientInfo == null || !GameManager.Instance.World.Players.dict.TryGetValue(clientInfo.entityId, out var player)) {
+                SdtdConsole.Instance.Output("Unable to find this player; note: player must be online");
+                return;
+            }
+            Config.SetLivesRemaining(player, livesRemaining);
+            SdtdConsole.Instance.Output($"Updated lives remaining for {player.GetDebugName()} to {livesRemaining}");
         }
     }
 }
--- OnlyThreeChances/Data/Values.cs
+++ OnlyThreeChances/Data/Values.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlyThreeChances.Data {
+﻿namespace OnlyThreeChances.Data {
     internal class Values {
         public static string LivesRemainingCvar { get; } = "livesRemaining";
         public static string MaxLivesCVar { get; } = "maxLives";
