5bcb585656233b3eb75a771ee6b068037c900278
jonathan-robertson <jrob356@icloud.com>
2022-06-28 08:49:15 -0500

    add api class, remove AudioModule ref

---
+++ OnlyThreeChances/API.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace OnlyThreeChances {
+    internal class API : IModApi {
+        public void InitMod(Mod _modInstance) {
+            throw new NotImplementedException();
+        }
+    }
+}
