e649799a169a40e362fed2accf663a85c93836fe
jonathan-robertson <jrob356@icloud.com>
2022-12-23 09:21:38 -0600

    update broadcast text ref for pos out award

--- src/Handlers/EntityKilled.cs
+++ src/Handlers/EntityKilled.cs
@@ -14,7 +14,7 @@ namespace Amnesia.Handlers {
                 }
 
                 var minutes = entry.value / 60f;
-                MessagingSystem.Broadcast($"[007fff]{killerEntity.GetDebugName()} just took down a {entry.name}!");
+                MessagingSystem.Broadcast($"[007fff]{killerEntity.GetDebugName()} just took down a {entry.caption}!");
                 MessagingSystem.Broadcast($"[007fff]Relief washes over each survivor as a newfound confidence takes hold: [00ff80]all online players receive Double XP for {(minutes > 1 ? minutes + " Minutes!" : entry.value + " Seconds!")}");
                 foreach (var player in GameManager.Instance.World.Players.list) {
                     _ = PlayerHelper.AddPositiveOutlookTime(player, entry.value);
